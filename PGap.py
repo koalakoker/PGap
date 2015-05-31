74523d871c932a3e280e4bf364e040d99cfc5912
Koalakoker <koala@koalakoker.com>
2015-05-31 15:29:40 +0200

    Used HSplit instead of HBox to resize it

--- PGap.py
+++ PGap.py
@@ -14,21 +14,22 @@ class NoteModel(gtk.TreeStore):
     newPageID = 2 #default value 1 is the welcome screen
     
     def __init__(self):
-        gtk.TreeStore.__init__(self, gobject.TYPE_STRING, gobject.TYPE_INT, gobject.TYPE_STRING, gobject.TYPE_STRING)
+        gtk.TreeStore.__init__(self, gobject.TYPE_STRING, gobject.TYPE_INT, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING)
         #init treestore with the following types
         # 0: String - Title of the note
         # 1: Int - ID of the note (unique ID inside the db)
         # 2: String - Time stamp (Creation)
         # 3: String - Time stamp (Last modification) 
+        # 4: String - Testo nota
         
     def populate(self):
         for parent in range(4):
             now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-            piter = self.append(None, ('parent %i' % parent, self.newPageID, now, now))
+            piter = self.append(None, ('parent %i' % parent, self.newPageID, now, now, ""))
             self.newPageID += 1
             for child in range(3):
                 now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-                self.append(piter, ('child %i of parent %i' % (child, parent), self.newPageID, now, now))
+                self.append(piter, ('child %i of parent %i' % (child, parent), self.newPageID, now, now, ""))
                 self.newPageID += 1
 
 class PGapMain:
@@ -43,6 +44,7 @@ class PGapMain:
         self.builder = gtk.Builder()
         self.builder.add_from_file(self.gladefile)
         self.window = self.builder.get_object("pgapui")
+        self.window.resize(1024,600)
         self.window.show_all()
         self.window.connect("delete_event", self.delete_event)
 
