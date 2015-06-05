55bc15135871a73759b35c40b1f78a6544773442
Koalakoker <koala@koalakoker.com>
2015-06-05 17:53:21 +0200

    Splitted NoteModel in a separate file

--- PGap.py
+++ PGap.py
@@ -8,45 +8,9 @@ import pygtk
 pygtk.require('2.0')
 import gtk
 from gtk import gdk
-import gobject
-import datetime
-from undobufferrich import undobufferrich
-import TextBuffer2HTMLConvert
+# import TextBuffer2HTMLConvert
 import pango
-
-initText = """Welcome to OGapp!
-
-OGapp is a slim sized and fast program to manage your textual notes. Notes are organized in pages and it is possible to setup a main password to get the access to the notes. When the notes are password protected the .ogp file in which the notes are stored will be encrypted.
-OGapp is cross platform and is available for Windows, Linux and (maybe in the future) Mac. It is based on Qt4 and is written in C++.
-
-This software is OPEN SOURCE and released under GPL license so you can feel FREE to use, copy, share, (but above all) to study, analyze and modify it as you like (within the terms of the license).
-If you like, hate or simply use this software, if you find any bug or have any request, please do not hesitate to let me know through the services offered by the site that hosts the project or through my Facebook page (http://facebook.com/koalakoker ). And (if you think it's the case) do not hesitate to recommend the program to your friends."""
-
-class NoteModel(gtk.TreeStore):
-    newPageID = 2 #default value 1 is the welcome screen
-    
-    def __init__(self, tagTable = None):
-        gtk.TreeStore.__init__(self, gobject.TYPE_STRING, gobject.TYPE_INT, gobject.TYPE_STRING, gobject.TYPE_STRING, gtk.TextBuffer)
-        #init treestore with the following types
-        # 0: String - Title of the note
-        # 1: Int - ID of the note (unique ID inside the db)
-        # 2: String - Time stamp (Creation)
-        # 3: String - Time stamp (Last modification) 
-        # 4: String - Testo nota
-        self.tagTable = tagTable
-        
-    def populate(self):
-        for parent in range(4):
-            now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-            it = ""
-            if (parent == 0):
-                it = initText
-            piter = self.append(None, ('parent %i' % parent, self.newPageID, now, now, undobufferrich(it, self.tagTable)))
-            self.newPageID += 1
-            for child in range(3):
-                now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-                self.append(piter, ('child %i of parent %i' % (child, parent), self.newPageID, now, now, undobufferrich("", self.tagTable)))
-                self.newPageID += 1
+from NoteModel import NoteModel
 
 class PGapMain:
     
@@ -263,8 +227,8 @@ class PGapMain:
 #         TextBuffer2HTMLConvert.toHTML(self.textbuffer)
 #         TextBuffer2HTMLConvert.serialize(self.textbuffer)
         self.NoteStore.populate()
-        diag = gtk.MessageDialog()
-        diag.show()
+#         diag = gtk.MessageDialog()
+#         diag.show()
     
 if __name__ == '__main__':
     main = PGapMain()
